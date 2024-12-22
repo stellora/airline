8636de1531d7b0ed018b85fa53b1735f6dbe5506
Quinn Slack <quinn@slack.org>
2024-12-22 01:46:31 -0600

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import type { PluginOption } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit() satisfies PluginOption as any],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
