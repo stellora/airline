62f6a40301c2841ab9c6cb2c4b6f6de5e7146c4c
Quinn Slack <quinn@slack.org>
2025-01-04 12:39:41 -0800

    wip

--- api-server/flight_instances.go
+++ api-server/flight_instances.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"log"
 
 	"github.com/stellora/airline/api-server/api"
 	"github.com/stellora/airline/api-server/db"
@@ -14,9 +13,6 @@ import (
 )
 
 func fromDBFlightInstance(a db.FlightInstancesView) api.FlightInstance {
-	log.Println(a.ID)
-	log.Println(a.ID, a.DepartureDatetime.Time.Location(), a.ArrivalDatetime.Time.Location())
-
 	// TODO!(sqs): bring in aircraft
 	b := api.FlightInstance{
 		Id: int(a.ID),
